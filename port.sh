870d5e36c3bd83379b09c26bbc6f2f897588215a
blahajcoding <venusisonvenus@gmail.com>
2026-04-02 15:10:29 +0100

    Added aria2c flag to prevent overwritting files I'm lazy, okay?

--- port.sh
+++ port.sh
@@ -48,7 +48,7 @@ fi
 # 检查为本地包还是链接
 if [ ! -f "${baserom}" ] && [ "$(echo $baserom |grep http)" != "" ];then
     blue "底包为一个链接，正在尝试下载" "Download link detected, start downloading.."
-    aria2c --max-download-limit=1024M --file-allocation=none -s10 -x10 -j10 ${baserom}
+    aria2c -c --max-download-limit=1024M --file-allocation=none -s10 -x10 -j10 ${baserom}
     baserom=$(basename ${baserom} | sed 's/\?t.*//')
     if [ ! -f "${baserom}" ];then
         error "下载错误" "Download error!"
